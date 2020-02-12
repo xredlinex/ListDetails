d87b58cf7fc0024324862d66537ca81fcc0ae70e
xredlinex <redik.line@gmail.com>
2020-02-12 13:32:20 +0200

    realm model

--- ListDetails/Model+Service/RealmModel.swift
+++ ListDetails/Model+Service/RealmModel.swift
@@ -7,3 +7,78 @@
 //
 
 import Foundation
+import UIKit
+import ObjectMapper
+import RealmSwift
+
+
+class NewsModel: Object, Mappable {
+    dynamic var status: String?
+    dynamic var totalResults: Int?
+    dynamic var articles = List<NewsArticlesModel>()
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    required init() {
+        super.init()
+    }
+    
+    func mapping(map: Map) {
+        status        <- map["status"]
+        totalResults  <- map["totalResults"]
+        articles      <- map["articles"]
+    }
+}
+
+class NewsArticlesModel: Object, Mappable {
+
+    @objc dynamic var sourse: NewsArticlesSourseModel?
+    @objc dynamic var author: String?
+    @objc dynamic var title: String?
+    @objc dynamic var newsDescription: String?
+    @objc dynamic var url: String?
+    @objc dynamic var urlToImage: String?
+    @objc dynamic var publishedAt: String?
+    @objc dynamic var content: String?
+    var showMoreDescript = false
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    required init() {
+        super.init()
+    }
+    
+    func mapping(map: Map) {
+        sourse              <- map["sourse"]
+        author              <- map["author"]
+        title               <- map["title"]
+        newsDescription     <- map["description"]
+        url                 <- map["url"]
+        urlToImage          <- map["urlToImage"]
+        publishedAt         <- map["publishedAt"]
+        content             <- map["content"]
+    }
+}
+
+class NewsArticlesSourseModel: Object, Mappable {
+ 
+    @objc dynamic var id: String?
+    @objc dynamic var name: String?
+    
+    required convenience init?(map: Map) {
+        self.init()
+     }
+    
+    required init() {
+        super.init()
+    }
+     
+    func mapping(map: Map) {
+        id      <- map["id"]
+        name    <- map["name"]
+     }
+}
